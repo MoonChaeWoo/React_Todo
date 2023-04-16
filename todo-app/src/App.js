b06f8b08fa404f944c198e898ff928343bf0069f
tyche0529 <tyche0529@gmail.com>
2023-04-16 11:33:51 +0900

    스타일 적용 및 반응형 적용
    & + &에 대한 문법에 대해서 알 수 있었음.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,12 @@
  import './App.css';
  import TodoInsert from './components/TodoInsert';
  import TodoList from './components/TodoList';
- import TodoListItem from './components/TodoListItem';
  import TodoTemplate from './components/TodoTemplate';
 
 function App() {
   return <TodoTemplate title="일정 관리">
             <TodoInsert/>
+            <TodoList/>
         </TodoTemplate>;
 }
 
--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -1,5 +1,14 @@
-const TodoList = () => {
+import '../scss/TodoList.scss'
+import TodoListItem from './TodoListItem';
 
+const TodoList = () => {
+    return(
+        <div className="TodoList">
+            <TodoListItem/>
+            <TodoListItem/>
+            <TodoListItem/>
+        </div>
+    );
 }
 
 export default TodoList;
